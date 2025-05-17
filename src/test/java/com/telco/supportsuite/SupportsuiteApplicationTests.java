00308c43aa136048bbd4357bba76e42ed637bea8
Julian Carvajal <julianc.carvajal12@gmail.coml>
2025-05-17 18:16:52 -0500

    chore: proyecto inicial generado con Spring Boot

---
+++ src/test/java/com/telco/supportsuite/SupportsuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telco.supportsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportsuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
