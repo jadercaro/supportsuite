aade1d0addf430f9a39ef0c25a75b2d85c337d24
julian <julianc.carvajal12@gmail.com>
2025-05-19 10:15:06 -0500

    feat: agregar UserEntity y RoleEntity con anotaciones JPA

---
+++ src/main/java/com/telco/users/RoleEntity.java
@@ -0,0 +1,45 @@
+package com.telco.users;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    // Constructor vacío
+    public RoleEntity() {
+    }
+
+    public RoleEntity(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
